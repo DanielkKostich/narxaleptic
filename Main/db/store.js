8977210b5dbff9cbbf6e60fffd72bb381ba93f81
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-06-22 15:03:25 -0500

    moved files

--- Main/db/store.js
+++ Main/db/store.js
@@ -9,11 +9,11 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
   read() {
-    return readFileAsync('db/db.json', 'utf8');
+    return readFileAsync('./db/db.json', 'utf8');
   }
 
   write(note) {
-    return writeFileAsync('db/db.json', JSON.stringify(note));
+    return writeFileAsync('./db/db.json', JSON.stringify(note));
   }
 
   getNotes() {
