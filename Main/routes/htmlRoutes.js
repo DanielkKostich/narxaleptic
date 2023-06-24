885172461c2b8f78bd6ba98dde1cbb5ec8f160fe
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-06-24 12:54:50 -0500

    fixed route

--- Main/routes/htmlRoutes.js
+++ Main/routes/htmlRoutes.js
@@ -7,7 +7,7 @@ router.get('/notes', (req, res) => {
 });
 
 // All other routes respond with the index.html file
-router.get('*', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
