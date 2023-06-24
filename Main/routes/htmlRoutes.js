52ca6c80fa13f8449eab6d239171e1d5c144a5da
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-06-24 12:57:14 -0500

    fixed route

--- Main/routes/htmlRoutes.js
+++ Main/routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 // "/notes" responds with the notes.html file
 router.get('/notes.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/.notes.html'));
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 // All other routes respond with the index.html file
