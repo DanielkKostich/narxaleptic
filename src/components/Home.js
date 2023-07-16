fd787383ceabe96281ac49c4dc97e64b26f09291
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-16 18:38:26 -0500

    added landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,54 @@
-import { Jumbotron, Container,  } from 'react-bootstrap';
+//import { Jumbotron, Container,  } from 'react-bootstrap';
 
 
 
 function Home() {
   return (
-    <div className = 'contact'>
-    <Jumbotron fluid className="hero text-white  header " >
-      <Container>
-        <h1>Welcome to Narxaleptic</h1>
-        <p>We specialize in building  custom cars.</p>
-        
-      </Container>
-    </Jumbotron>
-    
+    <div className = 'home'>
+      <header className = 'header'>
+        <h1>Welcome to Narxaleptics!</h1>
+      </header>
+
+      <section >
+        <h3>Our Services Include:</h3>
+        <br></br>
+        <h4>Customizations:</h4>
+        <p>From custom paint jobs to performance enhancements, our skilled technicians can bring your unique vision to life.</p>
+        <br></br>
+        <h3>Restorations:</h3>
+        <p>Preserve the timeless beauty of your classic car with our meticulous restoration services, combining originality with modern upgrades.</p>
+        <br></br>
+        <h3>Fabrication:</h3>
+        <p>Our experienced fabricators can craft bespoke components and perform body modifications tailored to your specifications.</p>
+        <br></br>
+        <h3>Performance Upgrades:</h3>
+        <p>Unlock the full potential of your vehicle with our performance upgrade solutions, optimizing power, handling, and tuning.</p>
+        <br></br>
+        <h3>Maintenance and Repairs:</h3>
+        <p>Ensure your vehicle operates at its best with our comprehensive maintenance services and timely repairs.</p>
+      </section>
+      <br></br>
+
+      <section >
+        <h1>Why Choose Us?</h1>
+        <br></br>
+        <h3>Expertise and Craftsmanship:</h3>
+        <p>Our passionate team of professionals delivers top-quality craftsmanship with meticulous attention to detail.</p>
+        <br></br>
+        <h3>Personalized Approach:</h3>
+        <p>We collaborate closely with you to understand your unique preferences and design customizations that reflect your style.</p>
+        <br></br>
+        <h3>Cutting-Edge Technology:</h3>
+        <p>We leverage advanced tools and diagnostic equipment to provide precise and efficient services.</p>
+        <br></br>
+        <h3>Customer Service:</h3>
+        <p>We prioritize exceptional customer service and strive to build lasting relationships with our clients.</p>
+      </section>
+
+      <footer>
+        <p>Visit us at 8512 Grady Lane in Decatur, Ar 72722 and let us transform your car into a true reflection of your passion and style.</p>
+        <p>Thank you for choosing Narxaleptic. We look forward to working with you!</p>
+      </footer>
     </div>
   );
 }
