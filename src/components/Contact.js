f8a56045e038a9c2cd6e67ddc72cfddd5de5a690
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-16 15:26:50 -0500

    modularized a bit, fixed links, and added preliminary style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,14 @@ function Contact() {
   return (
     <div>
       <h2>Contact Page</h2>
-      {/* Add your contact page content here */}
+      Paragraph 1:
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique elit id tortor ullamcorper, vel rutrum elit porta. Cras ultrices fermentum sem vitae convallis. Quisque a mauris fringilla, lacinia urna at, gravida leo. Vivamus mollis dignissim metus vitae tincidunt. Nam pulvinar felis vitae mauris pharetra, in sollicitudin ligula sagittis. Mauris dapibus magna sed odio dignissim facilisis. Suspendisse at neque massa. Nulla facilisi. Nunc non efficitur odio, ac congue velit. Nullam eget libero ut nisl aliquam vulputate. Sed vitae lorem hendrerit, pharetra purus nec, convallis nulla. Fusce venenatis, lectus a cursus finibus, dui ipsum feugiat mauris, a gravida ex risus vitae velit.
+
+Paragraph 2:
+In eu lacinia arcu, ut tempus purus. Maecenas bibendum, odio ut iaculis tincidunt, nisi eros volutpat massa, nec lobortis nunc sapien at enim. Sed tincidunt lacus quis dui sagittis, id varius ligula aliquet. Nunc ultrices luctus velit id congue. Quisque placerat diam quis enim laoreet, sed vulputate arcu viverra. Vivamus sollicitudin consectetur libero, ut commodo justo tincidunt ut. Nullam nec ipsum nec tellus cursus sollicitudin. Nullam ac fermentum velit. Sed auctor elit quis libero luctus, vitae rhoncus enim viverra. Suspendisse condimentum varius mi, et ultrices diam interdum sed. Proin vel est sit amet odio bibendum mattis sed ut ante.
+
+Paragraph 3:
+Praesent finibus nibh ac arcu ultrices rutrum. Curabitur pellentesque cursus ipsum vel lobortis. Quisque tincidunt metus nec sagittis posuere. Mauris eget enim id massa lobortis elementum. Sed volutpat interdum tellus, id ultrices turpis congue non. Integer sed suscipit purus, vel mattis lectus. Nulla facilisi. Etiam in lectus enim. Mauris placerat enim id turpis laoreet, vel elementum quam efficitur. Nam interdum ex id enim sollicitudin faucibus. Nullam auctor a metus a aliquet. Aliquam non diam magna. Vivamus dapibus, arcu id ultrices gravida, urna mi tincidunt neque, et condimentum lorem lacus vitae mi. Vestibulum vel odio vestibulum, condimentum arcu sed, ultrices purus. Aliquam ut magna vel odio tincidunt cursus.
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function NavBar({ currentPage, handlePageChange }) {
+  const handleLinkClick = (event, page) => {
+    event.preventDefault();
+    handlePageChange(page);
+  };
+
+  return (
+    <ul className="nav nav-pills nav-justified menu">
+      <li className="item">
+        <a
+          href="/"
+          onClick={(event) => handleLinkClick(event, 'Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="item">
+        <a
+          href="/gallery"
+          onClick={(event) => handleLinkClick(event, 'Gallery')}
+          className={currentPage === 'Gallery' ? 'nav-link active' : 'nav-link'}
+        >
+          Gallery
+        </a>
+      </li>
+      <li className="item">
+        <a
+          href="/contact"
+          onClick={(event) => handleLinkClick(event, 'Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="item">
+        <a
+          href="/about"
+          onClick={(event) => handleLinkClick(event, 'About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About Us
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavBar;
