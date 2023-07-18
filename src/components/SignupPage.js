61fc90220bb0a76af6b39e5836c9292d1694f5b3
Steve <stevemarti677@gmail.com>
2023-07-18 17:57:48 -0600

    fix

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import SignupPopup from './SignupPopup';
+
 
 const SignupPage = () => {
   return (
     <div className = 'gallery'>
       
-      {/* Render the SignupPopup component wherever you want */}
-      <SignupPopup />
+     
     </div>
   );
 };
