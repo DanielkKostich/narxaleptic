24912ba5009a158c3b715a9c2dcc909a5e3816db
Daniel <117228151+DanielkKostich@users.noreply.github.com>
2023-07-17 19:04:51 -0500

    created server file

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use('/graphql', graphqlHTTP({
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000/graphql');
+  console.log('Server is running on http://localhost:3000/graphql'); 
 });
